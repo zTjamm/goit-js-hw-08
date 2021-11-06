c75c9286b8a1ff5e352b4e9a03bd30ac1a34c973
zTjamm <jamm85@bk.ru>
2021-11-07 02:28:44 +0300

    galleru
    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,25 +6,18 @@ import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const gallery = document.querySelector('.gallery');
-const markup = galleryItems.map((img) => `<div class="gallery__item">
-  <a class="gallery__link" href="${img.original}">
+const markup = galleryItems.map((img) => `<li><a class="gallery__item" href="${img.original}">
     <img
       class="gallery__image"
-      src="${img.preview}"
-      data-source="${img.original}"
+      src="${img.preview}"      
       alt="${img.description}"
     />
-  </a>
-</div>`).join("");
-gallery.insertAdjacentHTML("afterbegin", markup)
-gallery.addEventListener("click", openModal);
-function openModal (event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
-    } else {
-        const urlOrig = event.target.dataset.source;
-        basicLightbox.create(`<img src="${urlOrig}">`).show();    
-    }     
-}
+  </a></li>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+let gallerys = new SimpleLightbox('.gallery a',
+            {                
+                captionsData: 'alt',
+                captionDelay: 250,                
+            });
+gallerys.on('show.simplelightbox');
 console.log(galleryItems);
