c1c100b4934b332c74451d8c403de98a7e2ab525
zTjamm <jamm85@bk.ru>
2021-11-07 04:41:08 +0300

    all done
    ok

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+const throttle = require('lodash.throttle');
+const TIME_DATA = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+let saveTime = 0;
+populateTime();
+player.setCurrentTime(saveTime);
+player.on('timeupdate', throttle(timeCurrenter, 1000));
+function timeCurrenter(el) {
+  localStorage.setItem(TIME_DATA, el.seconds);
+  saveTime = el.seconds;
+}
+function populateTime() {
+  if (localStorage.getItem(TIME_DATA)) {
+    saveTime = localStorage.getItem(TIME_DATA);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
-
+var throttle = require('lodash.throttle');
+const formContent = document.querySelector('.feedback-form');
+const email = formContent.querySelector('input');
+const message = formContent.querySelector('textarea');
+const FORM_DATA = 'feedback-form-state';
+const content = {
+  form: formContent,
+  email: email,
+  message: message,
+};
+let formObj = {};
+formInput();
+content.form.addEventListener('submit', submitForm);
+content.form.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput(e) {
+  formObj[e.target.name] = e.target.value;
+  localStorage.setItem(FORM_DATA, JSON.stringify(formObj));
+}
+function formInput() {
+  const savedForm = localStorage.getItem(FORM_DATA);
+  if (savedForm) {
+    const parsedForm = JSON.parse(savedForm);
+    if (parsedForm.email) {
+      data.email.value = parsedForm.email;
+      formObj.email =  data.email.value;
+    }
+    if (parsedForm.message) {
+      data.message.value = parsedForm.message;
+      formObj.message =  data.message.value;
+    }
+  }
+}
+function submitForm(e) {
+  e.preventDefault();
+  e.target.reset();
+  if (formObj.email&&formObj.message) {
+    localStorage.removeItem(FORM_DATA);
+  console.log(`Form Submited. Form is ${JSON.stringify(formObj)}`);
+  alert('форма отправлена');
+  formObj = {};
+  }
+  else {
+    alert('заполните поля');
+    formInput();
+  }
+}
